85a6173579701d40b1504c5e535019c136fb1b5f
Jan Hofmeier <jan.hofmeier@gmail.com>
2024-04-22 00:35:36 +0200

    use dynamic trampolines

--- source/main.c
+++ source/main.c
@@ -8,7 +8,7 @@
 #include <wafel/patch.h>
 #include <wafel/ios/svc.h>
 
-#include <loadfile.h>
+#include "loadfile_patch.h"
 
 // This fn runs before everything else in kernel mode.
 // It should be used to do extremely early patches
@@ -22,25 +22,7 @@ void kern_main()
 
     debug_printf("init_linking symbol at: %08x\n", wafel_find_symbol("init_linking"));
 
-    // // Loadfile patch
-    ASM_T_PATCH_K(0x050254D4, 
-        "ldr r0, _lf_hook\n"
-        "blx r0\n"
-        "_lf_hook: .word MCP_LoadFile_patch\n"
-    );
-
-
-    // ReadCOS patch
-    ASM_T_PATCH_K(0x0501DD74, 
-        "ldr r2, _cos_hook\n"
-        "blx r2\n"
-        "_cos_hook: .word MCP_ReadCOSXml_patch\n"
-    );
-    // ASM_T_PATCH_K(0x051105CA, 
-    //     "ldr r2, _cos_hook2\n"
-    //     "blx r2\n"
-    //     "_cos_hook2: .word MCP_ReadCOSXml_patch2\n"
-    // );
+    loadfile_install_patches();
 }
 
 // This fn runs before MCP's main thread, and can be used
