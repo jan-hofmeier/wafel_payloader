40d807d00096b780c189171a0d40917b33c14a2a
Jan Hofmeier <jan.hofmeier@gmail.com>
2024-07-13 18:22:39 +0200

    cleanup imports

--- source/loadfile.h
+++ source/loadfile.h
@@ -2,59 +2,6 @@
 
 #include <stdint.h>
 
-/* IPC message */
-typedef struct ipcmessage {
-    uint32_t command;
-    uint32_t result;
-    uint32_t fd;
-    uint32_t flags;
-    uint32_t client_cpu;
-    uint32_t client_pid;
-    uint64_t client_gid;
-    uint32_t server_handle;
-
-    union {
-        uint32_t args[5];
-
-        struct {
-            char *device;
-            uint32_t mode;
-            uint32_t resultfd;
-        } open;
-
-        struct {
-            void *data;
-            uint32_t length;
-        } read, write;
-
-        struct {
-            uint32_t offset;
-            uint32_t origin;
-        } seek;
-
-        struct {
-            uint32_t command;
-
-            uint32_t *buffer_in;
-            uint32_t length_in;
-            uint32_t *buffer_io;
-            uint32_t length_io;
-        } ioctl;
-        struct _ioctlv {
-            uint32_t command;
-
-            uint32_t num_in;
-            uint32_t num_io;
-            struct _ioctlv *vector;
-        } ioctlv;
-    };
-
-    uint32_t prev_command;
-    uint32_t prev_fd;
-    uint32_t virt0;
-    uint32_t virt1;
-} __attribute__((packed)) ipcmessage;
-
 typedef struct {
     unsigned char unk[0x10];
 
